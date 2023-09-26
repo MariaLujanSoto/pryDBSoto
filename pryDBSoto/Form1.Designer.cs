97e856b5109b5e50e18b0b20b8125e100d87cdd2
Alumnos <Alumnos@PC188>
2023-09-26 14:26:39 -0300

    Agregar archivos de proyecto.

---
+++ pryDBSoto/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace pryDBSoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(40, 35);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(93, 22);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "CONECTAR";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(40, 87);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(61, 13);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "-Resultado-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ pryDBSoto/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using System.Data.OleDb;
+
+namespace pryDBSoto
+{
+    public partial class Form1 : Form
+    {
+
+        OleDbConnection conexionBD; //declaro obj oled
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            conexionBD = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source=C:\\Users\\Alumnos\\source\\repos\\pryDBSoto\\BD\\2_EL_CLUB.accdb"); //poner tipo de arch
+            conexionBD.Open();
+
+            lblResultado.Text = "Conectado a la base";
+            lblResultado.BackColor = Color.Pink;
+        
+        
+        }
+
+    }
+
+
+}
+
